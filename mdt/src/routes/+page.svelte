459127c6ee3d7555cf78ce2739b808c7674573fe
LylaK <lylakirati@gmail.com>
2023-04-19 16:41:17 -0400

    added scrolly

--- mdt/src/routes/+page.svelte
+++ mdt/src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
+    import ScrollyTeller from '../components/ScrollyTeller.svelte'
     import App from '../components/App.svelte';
 </script>
 
-<App/>
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<ScrollyTeller />
+<!-- <App/> -->
+<!-- <h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
